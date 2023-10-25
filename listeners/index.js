93109abd92c8e25336f190a10ec078549928fcdb
Rishi Prakash Mishra <rishiprakash@aggregateintelligence.in>
2023-10-25 15:35:00 +0530

    shopName modification done

--- listeners/index.js
+++ listeners/index.js
@@ -72,53 +72,63 @@ module.exports = {
       if (shop === null) return logger.error("Invaild shop !")
       if (!user.faretrackToken) return logger.error("Faretracktoken not found !!")
       const dateRange = await horizonDateConverter(shop.horizons, "DATE_RANGE")
-      const docValue = await shop._OD.reduce(async (acc, cur) => [
-        {
-          ...acc,
-          fsid: null,
-          editshop: false,
-          shopname: shop.shopName,
-          flyfrom: cur._flyFrom.airportCode,
-          flyto: cur._flyTo.airportCode,
-          carriers: shop._carriers.map((c) => +c.code), // Pending
-          cabinclass: shop._cabinClasses.map((c) => c.code),
-          sources: shop._sources.map((s) => +s.code),
-          routetype: {
-            rt_type: shop.isRoundTrip ? 2 : 1,
-            los: shop.los !== 0 ? [shop.los] : null,
-            losval: shop.los !== 0 ? shop.los : null
-          },
-          los: null,
-          horizon: await horizonDateConverter(shop.horizons, "ONLY_SINGLE_DAY"),
-          horizon_days: await horizonDateConverter(shop.horizons, "DAY_RANGE"),
-          horizon_startday: 0,
-          horizon_endday: 0,
-          horizon_startdate: dateRange.startDate,
-          horizon_enddate: dateRange.endDate,
-          pax: {
-            adults: shop.pax.adults,
-            infants: shop.pax.infants
-          },
-          frequency: {
-            refresh: await scheduleConvert(doc.data.crontabExpression, "REFERSH"),
-            days: await scheduleConvert(doc.data.crontabExpression, "DAYS"),
-            time: [
-              {
-                starttime: await scheduleConvert(doc.data.crontabExpression, "STARTTIME"), // Pending ask murali
-                timezone: doc.data.timezoneName
-              }
-            ],
-            startdate: doc.data.startDate,
-            enddate: doc.data.endDate
-          },
-          pos: shop._pos.region,
-          currency: shop._currency.iso,
-          historical_horizon: 0,
-          historical_frequency: null,
-          rateshoptable: false,
-          visualizetable: user.isTablueUser
+      const docValue = await shop._OD.reduce(async (accPromise, cur, index) => {
+        try {
+          const acc = await accPromise
+          const carriers = shop?._carriers.map((c) => +c.code)
+          const cabinclass = shop?._cabinClasses.map((c) => c.code)
+          const sources = shop?._sources?.map((s) => +s.code)
+          return [
+            ...acc,
+            {
+              fsid: null,
+              editshop: false,
+              shopname: `${shop?.shopName}_${index}`,
+              flyfrom: cur?._flyFrom?.airportCode,
+              flyto: cur?._flyTo?.airportCode,
+              carriers,
+              cabinclass,
+              sources,
+              routetype: {
+                rt_type: shop.isRoundTrip ? 2 : 1,
+                los: shop.los !== 0 ? [shop.los] : null,
+                losval: shop.los !== 0 ? shop.los : null
+              },
+              los: null,
+              horizon: await horizonDateConverter(shop.horizons, "ONLY_SINGLE_DAY"),
+              horizon_days: await horizonDateConverter(shop.horizons, "DAY_RANGE"),
+              horizon_startday: 0,
+              horizon_endday: 0,
+              horizon_startdate: dateRange.startDate,
+              horizon_enddate: dateRange.endDate,
+              pax: {
+                adults: shop.pax.adults,
+                infants: shop.pax.infants
+              },
+              frequency: {
+                refresh: await scheduleConvert(doc.data.crontabExpression, "REFERSH"),
+                days: await scheduleConvert(doc.data.crontabExpression, "DAYS"),
+                time: [
+                  {
+                    starttime: await scheduleConvert(doc.data.crontabExpression, "STARTTIME"), // Pending ask murali
+                    timezone: doc.data.timezoneName
+                  }
+                ],
+                startdate: doc.data.startDate,
+                enddate: doc.data.endDate
+              },
+              pos: shop._pos.region,
+              currency: shop?._currency?.iso,
+              historical_horizon: 0,
+              historical_frequency: null,
+              rateshoptable: false,
+              visualizetable: user.isTablueUser
+            }
+          ]
+        } catch (error) {
+          return logger.error(error)
         }
-      ], [])
+      }, Promise.resolve([]))
       const marketArr = []
       // eslint-disable-next-line no-restricted-syntax
       for (const value of docValue) {
